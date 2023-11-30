a6935125821f50215683fed312a8af61ad10c381
againTraviscodes <t.uni.versity17@gmail.com>
2023-11-29 17:29:58 -0800

    a06 q02 approach implemented

--- Approach.py
+++ Approach.py
@@ -1,8 +1,8 @@
+import math
 from random import randint
 from random import random
-
-# Approach.py
-
+from enum import Enum
+import numpy as np
 
 # This program uses reinforcement learning to determine the optimal policy
 # for Approach.
@@ -15,17 +15,57 @@ from random import random
 # We can reduce this to the problem of player 1 choosing the best value at which to hold.
 # This is called a policy; once we know the best number to hold at, we can act optimally.
 
+epsilon = 0.1
+alpha = 0.1
+discount = 0.9
 
-def approach(n) :
+
+def approach(n):
     q_table = [[random() / 100.0, random() / 100.0] for i in range(n)]
+    hold = 0
+    roll = 1
 
-    for i in range(100000) :
+    for i in range(100000):
+        # print('\n')
         # Select an initial state.
+        s = randint(0, (n-1))
+        # print(s)
+        a = randint(hold, roll)
+
         # Take the best move with p=epsilon, and the worst move with p=1-epsilon.
         # Continue playing until the game is done.
+        while True:
+            if s == n:
+                break
+
+            if random() <= epsilon:
+                a = randint(hold, roll)
+            else:
+                a = hold if q_table[s][hold] >= q_table[s][roll] else roll
+
+            if a == hold:
+                break
+            else:
+                # s += randint(1, 6)
+                # if s >= n:
+                #     break
+                roll_val = randint(1, 6)
+                if s + roll_val >= n:
+                    #print('no addition')
+                    break
+                s += roll_val
+                #print('addition')
+
         # If you win, reward = 1.
         # If you lose, reward = 0.
+        r = 1 if s == n else 0
         # Use Q-learning to update the q-table for each state-action pair visited.
-        pass
+        # print(s)
+        val = q_table[s][a]
+        new_val = np.max(q_table[s])
+        q_table[s][a] = (1 - alpha) * val + alpha * (r + (1 - epsilon) * new_val)
 
+    action_sequence = ['roll' if q_table[i][roll] > q_table[i][hold] else 'hold' for i in range(n - 1)]
+    print(action_sequence)
+    return q_table
     # After 100000 iterations, print out your q-table.
--- submission.py
+++ submission.py
@@ -1,7 +1,12 @@
 from mdp import load_map_from_file
+from Approach import approach
 
 FILENAME = "rnGraph"
 
-g, t = load_map_from_file(fname=FILENAME)
+if __name__ == "__main__":
+    g, t = load_map_from_file(fname=FILENAME)
 
-print(t)
+    t10 = approach(10)
+    t12 = approach(12)
+    t18 = approach(18)
+    print(t10)
